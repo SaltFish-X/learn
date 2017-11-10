dbcc696f1f1dc03e3d2092d96b342cd44e8ced51
X-bear <522168288@qq.com>
2017-11-10 11:09:40 +0800

    add express-formmidable

--- learn-node/index.js
+++ learn-node/index.js
@@ -15,23 +15,31 @@ app.set('view engine', 'ejs')
 app.use(express.static(path.join(__dirname, 'public')))
 
 app.use(session({
-  // 设置 cookie 中保存session id的字段名称
-  name: config.session.key,
-  // 设置 secre 计算hash并放在cookie中，使产生的signedCookie防篡改
-  secret: config.session.secret,
-  // 强制更新 session
-  resave: true,
-  // 设置为 false，强制创建一个 session，即使用户未登录
-  saveUninitialized: false,
+  name: config.session.key,   // 设置 cookie 中保存session id的字段名称
+  secret: config.session.secret,   // 设置 secre 计算hash并放在cookie中，使产生的signedCookie防篡改
+  resave: true,   // 强制更新 session
+  saveUninitialized: false,   // 设置为 false，强制创建一个 session，即使用户未登录
   cookie: {
-    // 过期时间，过期后 cookie 中的 session id 自动删除
-    maxAge: config.session.maxAge
+    maxAge: config.session.maxAge     // 过期时间，过期后 cookie 中的 session id 自动删除
   },
-  // 将 session 存储到 mongodb
-  store: new MongoStore({ url: config.mongodb })
+  store: new MongoStore({ url: config.mongodb })   // 将 session 存储到 mongodb
 }))
 
 app.use(flash()) // flash 中间件，用来显示通知
+
+app.use(require('express-formidable')({
+  uploadDir: path.join(__dirname, 'public/img'),
+  keepExtensions: true
+}))
+
+app.locals.bloh = { title: pkg.name, description: pkg.description }
+
+app.use(function (req, res, next) {
+  res.locals.user = req.session.user
+  res.locals.success = req.flash('success').toString()
+  res.locals.error = req.flash('error').toString()
+  next()
+})
 
 routes(app)
 
