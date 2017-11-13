6b578057e28affeb6c5db038b2172c78b0f9e998
X-bear <522168288@qq.com>
2017-11-13 09:41:43 +0800

    update routes/signin.js

--- learn-node/models/user.js
+++ learn-node/models/user.js
@@ -3,5 +3,12 @@ var User = require('../lib/mongo').User
 module.exports = {
   create (user) {
     return User.create(user).exec()
+  },
+
+  getUserByName(name){
+    return User
+      .findOne({name:name})
+      .addCreateAt()
+      .exec()
   }
 }
--- learn-node/routes/signin.js
+++ learn-node/routes/signin.js
@@ -1,6 +1,8 @@
+var sha1 = require('sha1')
 var express = require('express')
 var router = express.Router()
 
+var UserModel = require('../models/user')
 var checkNotLogin = require('../middlewares/check').checkNotLogin
 
 // GET /signin 登陆页
@@ -10,7 +12,28 @@ router.get('/', checkNotLogin, function (req, res, next) {
 
 // POST /signin 用户登陆
 router.post('/', checkNotLogin, function (req, res, next) {
-  res.send(req.flash())
+  var name = req.fields.name
+  var password = req.fields.password
+
+  UserModel.getUserByName(name)
+    .then(function (user) {
+      if (!user) {
+        req.flash('error', 'user not existed')
+        return req.redirect('back')
+      }
+
+      if(sha1(password) !== user.password){
+        req.flash('error', 'password or username error')
+        return res.redirect('back')
+      }
+
+      req.flash('sucess', 'signin')
+      delete user.password
+      req.session.user = user
+
+      res.redirect('/posts')
+    })
+    .catch(next)
 })
 
 module.exports = router
