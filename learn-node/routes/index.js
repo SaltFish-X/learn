b49cb2de3afb6b0565c44eb7970ddd0f33c16d37
X-bear <522168288@qq.com>
2017-11-15 11:27:09 +0800

    Todo bug router 404

--- learn-node/routes/index.js
+++ learn-node/routes/index.js
@@ -1,14 +1,14 @@
 module.exports = function (app) {
-  app.get('/', function (req, res) {
-    res.redirect('/posts');
-  });
-  app.use('/signup', require('./signup'));
-  app.use('/signin', require('./signin'));
-  app.use('/signout', require('./signout'));
-  app.use('/posts', require('./posts'));
-  app.use((req, res) => {
-    if (!res.headerSent) {
-      res.status(404).render('404')
-    }
-  })
-};
+  app.get('/', (req, res) => { res.redirect('/signin') })
+  app.use('/signup', require('./signup'))
+  app.use('/signin', require('./signin'))
+  app.use('/signout', require('./signout'))
+  app.use('/posts', require('./posts'))
+  // app.use((req, res) => {
+  //   console.info(res.headerSent)
+  //   if (!res.headerSent) {
+  //     console.info('111') 
+  //     res.status(404).render('404')
+  //   }
+  // })
+}
