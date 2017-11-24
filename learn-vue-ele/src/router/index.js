fc3d227c0acf095fee47f62135fcd54a9749369e
X-bear <522168288@qq.com>
2017-11-24 15:08:38 +0800

    init views

--- learn-vue-ele/src/router/index.js
+++ learn-vue-ele/src/router/index.js
@@ -2,14 +2,17 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
 
+const Order = _ => import('@/views/order/order.vue')
+const Shop = _ => import('@/views/shop/shop.vue')
+const Home = _ => import('@/views/home/home.vue')
+
 Vue.use(Router)
 
 export default new Router({
   routes: [
-    {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    }
+    { path: '/', name: 'HelloWorld', component: HelloWorld },
+    { path: '/order', name: 'Order', component: Order },
+    { path: '/home', name: 'Home', component: Home },
+    { path: '/shop', name: 'Shop', component: Shop }
   ]
 })
